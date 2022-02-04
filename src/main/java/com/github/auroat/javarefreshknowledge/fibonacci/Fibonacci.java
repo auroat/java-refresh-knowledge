75d0b4395c62e7a52d6c5b46a2a50d0508204e83
Aurel Pintea <insanovation@gmail.com>
2022-02-04 15:55:20 +0000

    Add fibonacciSequenceByLimit method.

--- src/main/java/com/github/auroat/javarefreshknowledge/fibonacci/Fibonacci.java
+++ src/main/java/com/github/auroat/javarefreshknowledge/fibonacci/Fibonacci.java
@@ -4,4 +4,62 @@ public class Fibonacci {
     static int[] fibonacciSequenceOfLengthSeven() {
         return new int[]{0, 1, 1, 2, 3, 5, 8};
     }
+
+    static int[] fibonacciSequenceByLimit(Integer limit) {
+        if (limit == null || limit == 0) {
+            return null;
+        }
+
+        int[] result = new int[limit];
+        if (limit < 2) {
+            return result;
+        }
+
+        Tuple<Integer, Integer> latestFibonacciPair = new Tuple<>(0, 1);
+        result[0] = latestFibonacciPair.getFibonacciOne();
+        result[1] = latestFibonacciPair.getFibonacciTwo();
+
+        for (int i = 2; i < limit; i++) {
+            int currentFibonacciOne = latestFibonacciPair.getFibonacciOne();
+            int currentFibonacciTwo = latestFibonacciPair.getFibonacciTwo();
+
+            latestFibonacciPair.setFibonacciOne(currentFibonacciOne + currentFibonacciTwo);
+            latestFibonacciPair.setFibonacciTwo(latestFibonacciPair.getFibonacciOne() + currentFibonacciTwo);
+
+            result[i] = latestFibonacciPair.getFibonacciOne();
+            if (i + 1 < limit) {
+                result[i + 1] = latestFibonacciPair.getFibonacciTwo();
+            }
+
+            i++;
+        }
+
+        return result;
+    }
+}
+
+class Tuple<FibonacciOne, FibonacciTwo> {
+    private FibonacciOne fibonacciOne;
+    private FibonacciTwo fibonacciTwo;
+
+    public Tuple(FibonacciOne fibonacciOne, FibonacciTwo fibonacciTwo) {
+        this.fibonacciOne = fibonacciOne;
+        this.fibonacciTwo = fibonacciTwo;
+    }
+
+    public FibonacciOne getFibonacciOne() {
+        return fibonacciOne;
+    }
+
+    public void setFibonacciOne(FibonacciOne fibonacciOne) {
+        this.fibonacciOne = fibonacciOne;
+    }
+
+    public FibonacciTwo getFibonacciTwo() {
+        return fibonacciTwo;
+    }
+
+    public void setFibonacciTwo(FibonacciTwo fibonacciTwo) {
+        this.fibonacciTwo = fibonacciTwo;
+    }
 }
--- src/test/java/com/github/auroat/javarefreshknowledge/fibonacci/FibonacciTest.java
+++ src/test/java/com/github/auroat/javarefreshknowledge/fibonacci/FibonacciTest.java
@@ -19,10 +19,37 @@ public class FibonacciTest {
 
     @Tag("DEV")
     @Test
-    void testGenerateBasicFibonacciSequence() {
+    void generateBasicFibonacciSequence() {
         int[] result = Fibonacci.fibonacciSequenceOfLengthSeven();
         int[] expectedResult = {0, 1, 1, 2, 3, 5, 8};
 
+        Assertions.assertArrayEquals(expectedResult, result, "The two sequences should match.");
+    }
+
+    @Test
+    void fibonacciSequenceByLimit() {
+        // Test with limit 1.
+        int[] result = Fibonacci.fibonacciSequenceByLimit(1);
+        int[] expectedResult = {0};
+        Assertions.assertArrayEquals(expectedResult, result, "The two sequences should match.");
+
+        // Test with limit 10.
+        result = Fibonacci.fibonacciSequenceByLimit(10);
+        expectedResult = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+        Assertions.assertArrayEquals(expectedResult, result, "The two sequences should match.");
+
+        // Test with limit 7.
+        result = Fibonacci.fibonacciSequenceByLimit(7);
+        expectedResult = new int[]{0, 1, 1, 2, 3, 5, 8};
+        Assertions.assertArrayEquals(expectedResult, result, "The two sequences should match.");
+
+        // Test with limit 0.
+        result = Fibonacci.fibonacciSequenceByLimit(0);
+        expectedResult = null;
+        Assertions.assertArrayEquals(expectedResult, result, "The two sequences should match.");
+
+        // Test with limit NULL.
+        result = Fibonacci.fibonacciSequenceByLimit(null);
         Assertions.assertArrayEquals(expectedResult, result, "The two sequences should match.");
     }
 }
